a6dfe0e8f47fedb3a622ca68197274a71d0a1fbd
Banky <159575592+Azeezbank@users.noreply.github.com>
2026-01-29 16:07:14 +0100

    Add deleteAiSummary route to reportSummary

--- src/routes/reportSummary.route.ts
+++ src/routes/reportSummary.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { generateSummary, createAiSummary } from "../controller/reportSummary.controller.js"
+import { generateSummary, createAiSummary, deleteAiSummary } from "../controller/reportSummary.controller.js"
 
 
 const router = Router();
 
 router.post("/", createAiSummary);
 router.post("/:type", generateSummary);
+router.delete("/:id", deleteAiSummary);
 
 
 export default router;
